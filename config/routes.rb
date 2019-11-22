f77a1f212d272d953581809f16bf03e61e91172a
ozubergz <jmin499@gmail.com>
2019-11-22 09:44:45 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get '/posts', to: "posts#index"
+  
+  # get '/posts/:id', to: "posts#show"
+
   resources :posts, only: [:index, :show]
+
 end
